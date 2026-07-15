f67cb17814d1fb2acd80dbf3ba108e09168a6b6a
talaveron18 <talaveron18@gmail.com>
2026-07-15 19:08:45 +0200

    fix(netlify): next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: process.env.NETLIFY ? undefined : "standalone",
   /* config options here */
   reactStrictMode: false,
 };
